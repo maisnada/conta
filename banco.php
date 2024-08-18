d0b02ec25d11cef8757643b99bf66cd9ad643f90
Marcelo Moreno <marcelo.moreno@univali.br>
2024-08-17 22:25:11 -0300

    first commit

---
+++ banco.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'autoload.php';
+
+use Alura\Banco\Modelo\Conta\Titular;
+use Alura\Banco\Modelo\CPF;
+use Alura\Banco\Modelo\Endereco;
+use Alura\Banco\Modelo\Conta\Conta;
+
+$endereco = new Endereco('Itajaí', 'São João', 'Rua Antônio Ramos', '821');
+
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias', $endereco);
+$primeiraConta = new Conta($vinicius);
+$primeiraConta->deposita(500);
+$primeiraConta->saca(300); // isso é ok
+
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia',$endereco);
+$segundaConta = new Conta($patricia);
+var_dump($segundaConta);
+
